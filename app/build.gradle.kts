7c83d630849116aa8b27a49101e4ecaaff232109
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-11 22:22:27 +0300

    implemented glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.6.0")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+
+    //glide
+    implementation("com.github.bumptech.glide:glide:4.13.2")
 }
