b0350703691858c6070bb0e04105f3df59c1afdd
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-11 22:19:46 +0300

    added foods and response data classes

---
+++ app/src/main/java/com/elifoksas/foodorderingapp/data/entity/Foods.kt
@@ -0,0 +1,10 @@
+package com.elifoksas.foodorderingapp.data.entity
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Foods(
+    @SerializedName("yemek_id") var yemek_id : Int,
+    @SerializedName("yemek_adi") var yemek_adi : String,
+    @SerializedName("yemek_resim_adi") var yemek_resim_adi : String,
+    @SerializedName("yemek_fiyat") var yemek_fiyat : Int ) : Serializable {}
---
+++ app/src/main/java/com/elifoksas/foodorderingapp/data/entity/FoodsResponse.kt
@@ -0,0 +1,6 @@
+package com.elifoksas.foodorderingapp.data.entity
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class FoodsResponse(@SerializedName("yemekler")var foods:List<Foods>, @SerializedName("success")var success:Int) : Serializable
