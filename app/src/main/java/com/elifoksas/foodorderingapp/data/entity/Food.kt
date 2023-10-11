dd314102d7f2c0035b4d70c937d5c5dd2c6f81dc
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-11 22:20:39 +0300

    class name change

--- app/src/main/java/com/elifoksas/foodorderingapp/data/entity/Food.kt
+++
@@ -1,9 +0,0 @@
-package com.elifoksas.foodorderingapp.data.entity
-
-import java.io.Serializable
-
-data class Food(
-    var food_id : Int,
-    var food_name : String,
-    var food_image_name : String,
-    var food_price : Int ) : Serializable {}
