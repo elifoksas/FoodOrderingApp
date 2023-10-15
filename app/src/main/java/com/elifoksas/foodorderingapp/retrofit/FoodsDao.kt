03ba6c94046df8f230acba0ca252663dc0f5d75d
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-15 17:35:39 +0300

    added FoodsDao

--- app/src/main/java/com/elifoksas/foodorderingapp/retrofit/FoodsDao.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/retrofit/FoodsDao.kt
@@ -1,11 +1,35 @@
 package com.elifoksas.foodorderingapp.retrofit
 
+import com.elifoksas.foodorderingapp.data.entity.CRUDResponse
+import com.elifoksas.foodorderingapp.data.entity.CartFoodsResponse
 import com.elifoksas.foodorderingapp.data.entity.FoodsResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface FoodsDao {
-    //http://kasimadalan.pe.hu/yemekler/tumYemekleriGetir.php
 
     @GET("yemekler/tumYemekleriGetir.php")
     suspend fun loadFoods() : FoodsResponse
+
+    @POST("yemekler/sepeteYemekEkle.php")
+    @FormUrlEncoded
+    suspend fun addToCart(
+                          @Field("yemek_adi") food_name : String,
+                          @Field("yemek_resim_adi") food_image_name : String,
+                          @Field("yemek_fiyat") food_price : Int,
+                          @Field("yemek_siparis_adet") food_quantity : Int,
+                          @Field("kullanici_adi") username : String) : CRUDResponse
+
+
+    @POST("yemekler/sepettekiYemekleriGetir.php")
+    @FormUrlEncoded
+    suspend fun loadCart(@Field("kullanici_adi") username : String)  : CartFoodsResponse
+
+    @POST("yemekler/sepettenYemekSil.php")
+    @FormUrlEncoded
+    suspend fun deleteFood(
+        @Field("sepet_yemek_id") cart_food_id : Int,
+        @Field("kullanici_adi") username : String)  : CRUDResponse
 }
