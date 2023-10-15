330f960c79c0d728e072b1af7a17edcf70184cab
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-15 17:36:29 +0300

    changed repository and datasource

--- app/src/main/java/com/elifoksas/foodorderingapp/data/datasource/FoodDataSource.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/data/datasource/FoodDataSource.kt
@@ -1,5 +1,7 @@
 package com.elifoksas.foodorderingapp.data.datasource
 
+import android.util.Log
+import com.elifoksas.foodorderingapp.data.entity.CartFoods
 import com.elifoksas.foodorderingapp.data.entity.Foods
 import com.elifoksas.foodorderingapp.retrofit.FoodsDao
 import kotlinx.coroutines.Dispatchers
@@ -9,5 +11,24 @@ class FoodDataSource(var fdao:FoodsDao) {
 
     suspend fun loadFoods() : List<Foods> = withContext(Dispatchers.IO){
         return@withContext fdao.loadFoods().foods
+    }
+
+    suspend fun addToCart(food_name : String,
+                          food_image_name : String,
+                          food_price : Int,
+                          food_quantity : Int,
+                          username : String){
+        val response = fdao.addToCart(food_name,food_image_name,food_price,food_quantity,username)
+        Log.e("Add To Cart","Succes: ${response.success} - Message ${response.message}")
+    }
+
+    suspend fun loadCart(username : String) :List<CartFoods> = withContext(Dispatchers.IO){
+        val response = fdao.loadCart(username)
+        return@withContext response.cart_foods
+    }
+
+    suspend fun deleteFood(cart_food_id : Int, username : String){
+        val response = fdao.deleteFood(cart_food_id,username)
+        Log.e("Delete Food","Succes: ${response.success} - Message ${response.message}")
     }
 }
--- app/src/main/java/com/elifoksas/foodorderingapp/data/repository/FoodRepository.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/data/repository/FoodRepository.kt
@@ -1,10 +1,20 @@
 package com.elifoksas.foodorderingapp.data.repository
 
 import com.elifoksas.foodorderingapp.data.datasource.FoodDataSource
+import com.elifoksas.foodorderingapp.data.entity.CartFoods
 import com.elifoksas.foodorderingapp.data.entity.Foods
 
 class FoodRepository (var fds:FoodDataSource){
 
     suspend fun loadFoods() : List<Foods> = fds.loadFoods()
+    suspend fun addToCart(food_name : String,
+                          food_image_name : String,
+                          food_price : Int,
+                          food_quantity : Int,
+                          username : String) = fds.addToCart(food_name,food_image_name,food_price,food_quantity,username)
+
+    suspend fun loadCart(username : String) :List<CartFoods> = fds.loadCart(username)
+
+    suspend fun deleteFood(cart_food_id : Int, username : String) = fds.deleteFood(cart_food_id,username)
 
 }
