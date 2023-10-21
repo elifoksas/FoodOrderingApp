e4bdb2407cd54290344a0a2d43332de08568ef97
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-21 11:38:57 +0300

    configured liogin and signup process

--- app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/LoginFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.elifoksas.foodorderingapp.R
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.elifoksas.foodorderingapp.databinding.FragmentLoginBinding
+import com.elifoksas.foodorderingapp.ui.viewmodel.LoginViewModel
+import com.elifoksas.foodorderingapp.utils.gecis
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +25,24 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding =  FragmentLoginBinding.inflate(inflater, container, false)
+
+        viewModel.loginCheck.observe(viewLifecycleOwner){
+            if(it){
+                val transition = LoginFragmentDirections.actionLoginFragmentToHomePageFragment()
+                view?.let { it1 -> Navigation.gecis(it1,transition) }
+            }
+            else{
+                Toast.makeText(context,"Bilgileriniz Yanlış", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.signUpTextView.setOnClickListener {
+            val transition = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+            Navigation.gecis(it,transition)
+        }
+
+        binding.logInButton.setOnClickListener {
+            viewModel.login(binding.emailText.text.toString(),binding.emailPassword.text.toString())
+        }
 
 
         return binding.root
--- app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/SignUpFragment.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/SignUpFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.elifoksas.foodorderingapp.R
-import com.elifoksas.foodorderingapp.databinding.FragmentHomePageBinding
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.Navigation
 import com.elifoksas.foodorderingapp.databinding.FragmentSignUpBinding
+import com.elifoksas.foodorderingapp.ui.viewmodel.SignUpViewModel
+import com.elifoksas.foodorderingapp.utils.gecis
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SignUpFragment : Fragment() {
     private lateinit var binding: FragmentSignUpBinding
+    private val viewModel: SignUpViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,8 +27,33 @@ class SignUpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        viewModel.viewModelScope.launch {
+            if (viewModel.isValid()){
+                val transition = SignUpFragmentDirections.actionSignUpFragmentToHomePageFragment()
+                view?.let { it1 -> Navigation.gecis(it1,transition) }
+            }
+        }
 
 
+        binding.signUpButton.setOnClickListener {
+            viewModel.signUp(binding.emailText.text.toString(),binding.emailPassword.text.toString())
+        }
+
+
+        viewModel.signUpCheck.observe(viewLifecycleOwner){
+            if(it){
+                val transition = SignUpFragmentDirections.actionSignUpFragmentToHomePageFragment()
+                view?.let { it1 -> Navigation.gecis(it1,transition) }
+            }
+            else{
+                Toast.makeText(context,"Bilgileriniz Yanlış",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.loginTextView.setOnClickListener {
+            val transition = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
+            view?.let { it1 -> Navigation.gecis(it1,transition) }
+        }
         return binding.root
     }
 
