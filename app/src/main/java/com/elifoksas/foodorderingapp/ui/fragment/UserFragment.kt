925ab007cbd83eb2644439c2208219f45b1688ba
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-21 11:35:31 +0300

    added logout process

--- app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/UserFragment.kt
+++ app/src/main/java/com/elifoksas/foodorderingapp/ui/fragment/UserFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.elifoksas.foodorderingapp.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.elifoksas.foodorderingapp.databinding.FragmentUserBinding
+import com.elifoksas.foodorderingapp.ui.viewmodel.UserViewModel
+import com.elifoksas.foodorderingapp.utils.gecis
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UserFragment : Fragment() {
 
     private lateinit var binding: FragmentUserBinding
+    private val viewModel: UserViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,18 @@ class UserFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding =  FragmentUserBinding.inflate(inflater, container, false)
+        binding.logOutButton.setOnClickListener {
+            viewModel.logOut()
+        }
+        viewModel.logOutCheck.observe(viewLifecycleOwner){
+            if (!it){
+                val transition = UserFragmentDirections.actionUserFragmentToSignUpFragment()
+                view?.let { it1 -> Navigation.gecis(it1,transition) }
+            }
+            else{
+
+            }
+        }
 
 
         return binding.root
