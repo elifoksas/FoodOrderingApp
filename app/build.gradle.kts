ef73fed20368a870d683e47b5ea659ebb38353d9
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-21 11:33:28 +0300

    implemented firebase database and firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore")
+
+
 }
